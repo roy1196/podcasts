9d073dd598b36fdd90d6b78919a1b6a4a18f4846
Roy Hodson <royhg1496@gmail.com>
2019-12-05 11:17:16 -0600

    [update] proyecto terminado

--- pages/channel.js
+++ pages/channel.js
@@ -3,7 +3,14 @@ import Layout from "../components/Layout";
 import ChannelGrid from "../components/ChannelGrid";
 import PodcastList from "../components/PodcastList";
 import Error from "./_error";
+import PodcastListWithClick from "../components/PodcastListWithClick";
+import PodcastPlayer from "../components/PodcastPlayer";
 export default class extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {openPodcast: null}
+  }
   //El server guarda el query argument
   static async getInitialProps({ res, query }) {
     try{
@@ -56,13 +63,43 @@ export default class extends React.Component {
     }
 
   }
+
+  openPodcast = (event, podcast) => {
+    if ( event.metaKey || event.ctrlKey || event.shiftKey || (event.nativeEvent && event.nativeEvent.which === 2) ) {
+      // Si está haciendo Ctrl+Click o Cmd+Click, dejamos que el click suceda normalmente
+      return
+    }
+
+    event.preventDefault()
+    this.setState({
+      openPodcast: podcast
+    })
+  }
+
+  closePodcast = (event) => {
+    event.preventDefault()
+    this.setState({
+      openPodcast: null
+    })
+  }
+
   render() {
     const { channel, audioclips, series, statusCode } = this.props;
+    const {openPodcast} = this.state;
+
+    
+
     if( statusCode !== 200 ) {
       return <Error statusCode={ statusCode } />
     }
     return (
       <Layout title={channel.title}>
+       { openPodcast && 
+        <div className="modal">
+          <PodcastPlayer clip={ openPodcast } onClose={ this.closePodcast } />
+        </div>
+      }
+
       <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
       
       <h1>{ channel.title }</h1>
@@ -75,7 +112,7 @@ export default class extends React.Component {
       }
 
       <h2>Ultimos Podcasts</h2>
-      <PodcastList podcasts={ audioclips } />
+      <PodcastListWithClick podcasts={ audioclips }  onClickPodcast={this.openPodcast}/>
 
       <style jsx>{`
         .banner {
@@ -94,6 +131,15 @@ export default class extends React.Component {
           font-size: 1.2em;
           font-weight: 600;
           margin: 0;
+        }
+        .modal{
+          position:fixed;
+          top:0;
+          left:0;
+          right:0;
+          bottom:0;
+          z-index:9999;
+          background-color:red;
         }
       `}</style>
     </Layout>
